b3dd254e4bc776c2e4555c066056828812bc5294
DressofBD <47344807+DressofBD@users.noreply.github.com>
2020-07-24 20:01:41 +0600

    Update main.js

--- 3/web/assets/js/main.js
+++ 3/web/assets/js/main.js
@@ -1,8 +1,30 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 150){  
-        $('header').addClass("sticky");
-    }
-    else{
-        $('header').removeClass("sticky");
-    }
+
+
+$( document ).ready(function() {
+			 var shrinkHeader = 150;
+			  $(window).scroll(function() {
+				var scroll = getCurrentScroll();
+				  if ( scroll >= shrinkHeader ) {
+					   $('header').addClass('sticky');
+					}
+					else {
+						$('header').removeClass('sticky');
+					}
+			  });
+			function getCurrentScroll() {
+				return window.pageYOffset;
+				}
+			});
+
+
+document.querySelectorAll('a[href^="#approach"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
+
+
