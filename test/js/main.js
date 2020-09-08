794348f76e9ab747af885650e9e19451e7ea2c00
DressofBD <47344807+DressofBD@users.noreply.github.com>
2020-09-08 15:03:15 +0600

    Add files via upload

---
+++ test/js/main.js
@@ -0,0 +1,46 @@
+$('#carousel1').owlCarousel({
+    margin:10,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    loop:true,
+    responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:true
+        },
+        1000:{
+            items:3,
+            nav:true,
+        }
+    }
+});
+
+
+$('#carousel2').owlCarousel({
+    margin:10,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    loop:true,
+    responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:true
+        },
+        1000:{
+            items:4,
+            nav:true,
+        }
+    }
+});
